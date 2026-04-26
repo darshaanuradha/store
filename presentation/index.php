c7d0c22bbc204689eac66006a8a47302b5271c39
imlakmali <koralagelal573@gmail.com>
2026-04-26 14:28:20 +0530

    fetched data in view

---
+++ presentation/index.php
@@ -0,0 +1,39 @@
+<?php
+require_once '../application/db.php';
+
+//fetch data from database
+$query = "SELECT * FROM products";
+$result = mysqli_query($conn, $query);
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Products</h1>
+    <table border="1">
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Price</th>
+            <th>Quantity</th>
+        </tr>
+        <?php while($row = mysqli_fetch_assoc($result)) { ?>
+        <tr>
+            <td><?php echo $row['id']; ?></td>
+            <td><?php echo $row['name']; ?></td>
+            <td><?php echo $row['description']; ?></td>
+            <td><?php echo $row['price']; ?></td>
+            <td><?php echo $row['quantity']; ?></td>
+        </tr>
+        <?php } ?>
+    </table>
+</body>
+</html>
